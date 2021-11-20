3a6d87223f219a50a501d5b4cf5d07b51be6cf9b
Trần Anh Tuấn <83346864+Tunasss@users.noreply.github.com>
2021-11-20 09:45:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     titleWeb: "CHÚC MỪNG 20-11",
     introTitle: 'LỚP 10 TIN GỬI CÁC THẦY CÔ',
-    introDesc: `Dù học với thầy cô vài tháng, nhưng chúng em đã học rất vui, dù có ngày lớp quậy, lớp sôi nổi, lớp trầm, nhưng dù chỉ học qua màn ảnh nhỏ, bọn em vẫn rất yêu quý thầy cô. Chúng em không có gì hơn ngoài chúc thầy cô có một 20-11 thật là vui vẻ, và một sức khỏe thật dồi dào, để có thể dìu dắt chúng em hết 3 năm cấp 3 này ạ. Cảm ơn các thầy cô - những người lái đò tận tụy đã từng bước đưa chúng em qua bến bờ bên kia trong suốt thời gian vừa qua. Chúc các thầy cô luôn đủ tâm - trí - lực để hoàn thành nhiệm vụ của mình nuôi dưỡng tương lai đất nước cống hiến nhiều hơn cho sự nghiệp cao cả và vĩ đại này. From 10 Tin with love <3`,
+    introDesc: `Dù học với thầy cô vài tháng, nhưng chúng em đã học rất vui, dù có ngày lớp quậy, lớp sôi nổi, lớp trầm, nhưng dù chỉ học qua màn ảnh nhỏ, bọn em vẫn rất yêu quý thầy cô. Chúng em không có gì hơn ngoài chúc thầy cô có một 20-11 thật là vui vẻ, và một sức khỏe thật dồi dào, để có thể dìu dắt chúng em hết 3 năm cấp 3 này ạ. From 10 Tin with love <3`,
 }
